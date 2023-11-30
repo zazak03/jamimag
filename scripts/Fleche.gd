ba370ae461167a2b4b8331b86553469d83abde1a
Tristan <titusbinus@gmail.com>
2023-11-30 18:12:43 +0100

    Deplacement et debut hitbox

---
+++ scripts/Fleche.gd
@@ -0,0 +1,22 @@
+extends Sprite2D
+var speed = 200
+var rot_speed = PI/3 + PI/4
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var sign_rot = 0
+	if Input.is_action_pressed("turn_counterclockwise"):
+		sign_rot = -1
+	if Input.is_action_pressed("turn_clockwise"):
+		sign_rot = 1
+	rotation += rot_speed * delta * sign_rot
+	var velocity = Vector2.RIGHT.rotated(rotation) * speed
+	position += velocity * delta
+
+
+	
