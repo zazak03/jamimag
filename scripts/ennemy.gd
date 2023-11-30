6ff169a6d9d3e99c1da9ba135a33f50ddb8cbc00
zazak03 <zazak.pascal@gmail.com>
2023-11-30 19:44:13 +0100

    jolie annimation

--- scripts/ennemy.gd
+++ scripts/ennemy.gd
@@ -6,8 +6,8 @@ var bullet_scene = preload("res://scenes/example.tscn")
 
 func tirer():
 	var bullet_instance = bullet_scene.instantiate()
-	bullet_instance.global_position = $bullet_start.global_position
-	bullet_instance.global_rotation = global_rotation
+	bullet_instance.global_position = $ennemy_oriantation/bullet_start.global_position
+	bullet_instance.global_rotation = $ennemy_oriantation.global_rotation
 	add_child(bullet_instance)
 	
 
@@ -20,9 +20,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate(get_angle_to(target.global_position))
-	print(get_angle_to(target.global_position))
-	pass
+	global_rotation = 0
+	$ennemy_oriantation.rotate(get_angle_to(target.global_position))
 
 
 func _on_timer_timeout():
