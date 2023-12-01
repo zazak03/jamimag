a8a573068fb6b38f25e828ef4fcb8d2e0076acdb
zazak03 <zazak.pascal@gmail.com>
2023-12-02 00:05:05 +0100

    la cape march

---
+++ fleche_complet.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@export var nbr_points_cape = 22
+@export var update_interval = 0.1
+@export var cape = preload("res://scenes/cape.tscn")
+var points_cape = []
+var derniers_vect_pos = []
+@onready var ma_var = $fleche
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in range(nbr_points_cape):
+		$DepCape.add_child(cape.instantiate())
+	for child in $DepCape.get_children():
+		points_cape.push_back(child)
+	$Timer.wait_time = update_interval
+	$Timer.start(0)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	pass
+
+
+func update_pos():
+	points_cape.push_front(points_cape.pop_back())
+	points_cape[1].position = $fleche.position
+	print(points_cape[1].global_position)
+	print($fleche/Camera.global_position)
+
+
+func _on_timer_timeout():
+	update_pos()
--- scripts/Fleche.gd
+++ scripts/Fleche.gd
@@ -2,21 +2,10 @@ extends Area2D
 @export var speed = 200
 @export var rot_speed = PI/3 + PI/4
 signal  Fleche_hit
-@export var nbr_points_cape = 22
-@export var ecart_points = 10
-@export var cape = preload("res://scenes/cape.tscn")
-@onready var depart_cape = $DepCape
-var points_cape = []
-var derniers_vect_pos = []
 
 # Called when the node enters the scene tree for the first time.
 #CLE: instancien nbr_points_cape instances du point de base de la cape
 func _ready():
-	for i in range(nbr_points_cape):
-		depart_cape.add_child(cape.instantiate())
-		for child in depart_cape.get_children():
-			points_cape.push_back(child)
-			derniers_vect_pos.push_back(child.position)
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,8 +20,7 @@ func _process(delta):
 	position += velocity * delta
 
 func _physics_process(_delta):
-	depart_cape.rotation = 0
-	update_pos()
+	pass
 
 #CLE: Insère un élément dans un tableau rempli, le dernier élément finit hors du tableau.
 func decale(val, tabl):
@@ -54,12 +42,10 @@ func decale2(val, tab):
 #CLE: Fonction qui s'occupe de décaler tous les points de la cape
 #Pour Tristan : il faut que tu mette DepCape dans la scène principale, de manière à ce qu'elle suive
 #la position de fleche mais n'hérite pas de sa rotation 
-func update_pos():
-	var pos_actuelle = position - depart_cape.position
-	decale2(Vector2.ZERO, derniers_vect_pos)
-	derniers_vect_pos = derniers_vect_pos.map(func(vector2):return vector2 - position.normalized()*ecart_points)
-	for i in range(depart_cape.get_child_count()):
-		depart_cape.get_child(i).position = derniers_vect_pos[i]
+
+	#derniers_vect_pos = derniers_vect_pos.map(func(vector2):return vector2 - position.normalized()*ecart_points)
+	#for i in range(depart_cape.get_child_count()):
+	#	depart_cape.get_child(i).position = derniers_vect_pos[i]
 		
 
 
