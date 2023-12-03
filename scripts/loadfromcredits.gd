a7ad5855899b6e32bb0a63969235be0873f4e3ad
Tristan <titusbinus@gmail.com>
2023-12-03 15:13:59 +0100

    remise a 0 modif

--- scripts/loadfromcredits.gd
+++ scripts/loadfromcredits.gd
@@ -4,11 +4,9 @@ extends Node2D
 @export var worstlevel:PackedScene
 @export var modesurvie:PackedScene
 
-
-func _on_worst_button_pressed():
+func _on_worst_pressed():
 	get_tree().change_scene_to_packed(worstlevel)
 
 
-
-func _on_survivalbutton_pressed():
+func _on_survival_pressed():
 	get_tree().change_scene_to_packed(modesurvie)
