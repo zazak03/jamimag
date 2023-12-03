08b7057509fb8fec87eaf1964faf3410e14cf59f
zazak03 <zazak.pascal@gmail.com>
2023-12-03 10:38:52 +0100

    cape plus jolie

--- scripts/fleche_complet.gd
+++ scripts/fleche_complet.gd
@@ -7,14 +7,17 @@ var points_cape = []
 var derniers_vect_pos = []
 @onready var ma_var = $fleche
 
+var i_cape = 0
+
 signal fleche_hit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in range(nbr_points_cape):
-		$DepCape.add_child(cape.instantiate())
-	for child in $DepCape.get_children():
+		var child = cape.instantiate()
+		$PhysiqueCape.add_child(child)
 		points_cape.push_back(child)
+		$Visual_cape.add_point($fleche.position)
 	$Timer.wait_time = update_interval
 	$Timer.start(0)
 
@@ -25,16 +28,18 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-	pass
+	update_pos()
 
 
 func update_pos():
-	points_cape.push_front(points_cape.pop_back())
-	points_cape[1].position = $fleche.position
+	points_cape[i_cape].position = $fleche.position
+	$Visual_cape.remove_point(0)
+	$Visual_cape.add_point($fleche.position)
+	i_cape = (i_cape + 1) % nbr_points_cape
 
 
 func _on_timer_timeout():
-	update_pos()
+	pass
 
 
 
